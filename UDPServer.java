32db752a6ed90ee5032ac05f3949804f889a633f
strawberrypie <kjdr@kth.se>
2014-09-11 19:01:06 +0200

    Handshake, still no exception handler

--- UDPServer.java
+++ UDPServer.java
@@ -1,3 +1,11 @@
+/*
+ * 
+ * Simple UDP connection program
+ * Created by Karl-Johan Drougge and Danijel Kljakic
+ * 2014-09-11
+ * 
+ * 
+ */
 
 import java.io.*;
 import java.net.*;
@@ -5,14 +13,12 @@ import java.util.*;
 
 public class UDPServer {
 
-	//private static InetAddress currentClientIP;
-	//private static int currentClientPort;
 	private static String answer;
 	private static int randomNumber;
 	
 	
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
+		
 		DatagramSocket serverSocket = new DatagramSocket(9876);
 		DatagramPacket receivePacket;
 		DatagramPacket sendPacket;
@@ -21,7 +27,6 @@ public class UDPServer {
 		InetAddress currentClientIP = null;
 		int currentClientPort = 0;
 		
-		
 		String helloMsg = "HELLO";
 		String okMsg = "OK";
 		String startMsg = "START";
@@ -29,60 +34,76 @@ public class UDPServer {
 		String busyMsg = "BUSY";
 		String errorMsg = "ERROR";
 		
-		
-		//init();
-		
+		boolean isGameStarted = false;
+
 		byte[] receiveData = new byte[1024];
 		byte[] sendData = new byte[1024];
-		
+
 		while(true){
-			
 			receivePacket = new DatagramPacket(receiveData, receiveData.length);
 			serverSocket.receive(receivePacket);
-			System.out.println("VEM:" + receivePacket.getSocketAddress());
+		
+			System.out.println("WHO:" + receivePacket.getSocketAddress());
 			
 			IPAddress = receivePacket.getAddress();
 			port = receivePacket.getPort();
 			
+			String initMsg = new String(receivePacket.getData()).trim();
+			
 			if(currentClientIP == null){
-				currentClientIP = IPAddress;
-				currentClientPort = port;
-			}
-			
-			if(currentClientIP.equals(IPAddress) && currentClientPort == port) {
-				int guessedNumber = Integer.parseInt(new String(receivePacket.getData()).trim());
-				
-				System.out.println("RECEIVED: " + guessedNumber);
-			
-				if(randomNumber > guessedNumber){
-					answer = "LO";
-				} else if(randomNumber < guessedNumber){
-					answer = "HI";
+				if(initMsg.equals(helloMsg)){
+					currentClientIP = IPAddress;
+					currentClientPort = port;
+					sendData = okMsg.getBytes();
+					sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
 				} else {
-					answer = "CORRECT";
+					sendData = errorMsg.getBytes();
+					sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
 				}
+			} else if(currentClientIP.equals(IPAddress) && currentClientPort == port) {
+				if(!isGameStarted){
+					String startClientMsg = new String(receivePacket.getData()).trim();
+					if(startClientMsg.equals(startMsg)){
+						startGame();
+						sendData = readyMsg.getBytes();
+						sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+						isGameStarted = true;
+					} else {
+						sendData = errorMsg.getBytes();
+					}
+					
+					sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+				} else{
+					int guessedNumber = Integer.parseInt(new String(receivePacket.getData()).trim());
+					System.out.println("RECEIVED: " + guessedNumber);
 			
-				sendData = answer.getBytes();
-				sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-			
-				receiveData = new byte[1024];
-				sendData = new byte[1024];
-			
-				serverSocket.send(sendPacket);
+					if(randomNumber > guessedNumber){
+						answer = "LO";
+					} else if(randomNumber < guessedNumber){
+						answer = "HI";
+					} else {
+						answer = "CORRECT";
+					}
+					
+					sendData = answer.getBytes();
+					sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+				
+					if(answer.equals("CORRECT")){
+						currentClientIP = null;
+						currentClientPort = 0;
+						isGameStarted = false;
+					}
+				}
+				
 			} else {
 				sendData = busyMsg.getBytes();
 				sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-				serverSocket.send(sendPacket);
 			}
 			
+			serverSocket.send(sendPacket);
+			receiveData = new byte[1024];
+			sendData = new byte[1024];
 		}
-	}
-	
-	public static void init(){
-		byte[] receiveData = new byte[1024];
-		byte[] sendData = new byte[1024];
-		
-		
 	}
 	
 	public static void startGame(){
