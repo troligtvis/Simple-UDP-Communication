40972a9537303158ba102d42a52cc329e59e74b0
dancii <gromati163@gmail.com>
2014-09-11 18:20:05 +0200

    Exceptions
    Client exceptions

--- UDPClient.java
+++ UDPClient.java
@@ -5,24 +5,32 @@ public class UDPClient {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		//hej
 		
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		DatagramSocket clientSocket = new DatagramSocket();
 		InetAddress IPAddress = InetAddress.getByName("localhost");
-		while(true){
-		byte[] sendData = new byte[1024];
-		byte[] receiveData = new byte[1024];
-		String sentence = inFromUser.readLine();
-		sendData = sentence.getBytes();
-		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-		clientSocket.send(sendPacket);
-		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-		clientSocket.receive(receivePacket);
-		String modifiedSentence = new String(receivePacket.getData());
-		System.out.println("FROM SERVER: " + modifiedSentence);
+		try{
+			while(true){
+				byte[] sendData = new byte[1024];
+				byte[] receiveData = new byte[1024];
+				String sentence = inFromUser.readLine();
+				sendData = sentence.getBytes();
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+				clientSocket.send(sendPacket);
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				clientSocket.receive(receivePacket);
+				String modifiedSentence = new String(receivePacket.getData());
+				System.out.println("FROM SERVER: " + modifiedSentence);
+			}
+		}catch(SocketException e){
+			System.out.println("Socket error: " + e.getMessage());
+		}catch(IOException e){
+            System.out.println("IO: " + e.getMessage());
+		}finally{
+			if(clientSocket!=null){
+				clientSocket.close();
+			}
 		}
-		
 		//clientSocket.close();
 	}
 
