a542f5f4a7a51ef491ab63608ca69dee06bddaa7
dancii <gromati163@gmail.com>
2014-09-11 18:00:02 +0200

    Kommentar
    Se om det funkar

--- UDPClient.java
+++ UDPClient.java
@@ -5,7 +5,7 @@ public class UDPClient {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
+		//hej
 		
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		DatagramSocket clientSocket = new DatagramSocket();
